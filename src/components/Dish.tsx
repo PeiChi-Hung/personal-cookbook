2389ca18ff5ce19b4878a8644a29a0aaad71f344
Patty Hung <pattyhung72@gmail.com>
2024-02-09 23:01:32 +1100

    feat: link a monogodb

--- src/components/Dish.tsx
+++ src/components/Dish.tsx
@@ -5,8 +5,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 export default function Dish() {
+  const useRecipe = useQuery({
+    queryKey: ["recipes"],
+    queryFn: async () => {
+      const response = await axios.get("/.netlify/functions/mogo")
+      return response.data
+    },
+  })
+
+  console.log(useRecipe.data[0])
+
   return (
     <Card className="w-[350px] m-10">
       <CardHeader>
